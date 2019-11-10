359310cab2606e3553ef4ab3ac08d33b86fc5c4f
Nima Boscarino <nima.boscarino@gmail.com>
2019-11-10 05:13:50 -0800

    sockets

--- src/App/Socket.js
+++ src/App/Socket.js
@@ -11,21 +11,22 @@ class Socket {
   }
   login = (id) => {
     cookies.set('id', id);
-    this.socket = io('http://localhost',  { query: `id=${cookies.id}` });
+    this.socket = io('http://localhost:3003',  { query: `id=${cookies.id}` });
     this.socketState = this.socket;
-    this.socket.on ('shunt', function (data) {
-      this.socketState = io(`http://localhost/${data.namespace}`);
+    this.socket.on ('shunt', (data) => {
+      this.socketState = io(`http://localhost:3003/${data.namespace}`);
       this.bus = data.busId;
       this.socket.emit('join', {busId: this.bus});
     });
     setInterval(this.update, 2000);
   }
   update = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((res) => {
-        this.socketState.emit ('update', {id: cookies.id, busId: this.bus, coords: res.coords});
-      });
-    }
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition((res) => {
+      console.log('asdasdas')
+    this.socket.emit ('update', {id: cookies.id, busId: this.bus, coords: []});
+    //   });
+    // }
   }
   subscribe = (respond) => {
     respond('joust', {name: 'Joust', varient: 'Peds'});
